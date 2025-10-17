910cafc7e50b9fafa9aea2d2cd530f7352f612f2
mark <it.emc2015@gmail.com>
2025-10-17 13:38:19 +0800

    switch login page on home page

--- auth.py
+++ auth.py
@@ -1,4 +1,19 @@
-from flask import Flask, render_template,url_for,redirect,request,Blueprints
+from flask import Flask, render_template,url_for,redirect,request,Blueprint,current_app,flash
 
-auth = Blueprints('auth',__name__)
+auth = Blueprint('auth',__name__)
 
+@auth.route('/sign',methods=['POST','GET'])
+def signup():
+    if request.method == 'POST':
+        t_db = current_app.config['userdb']
+        email = request.form['email']
+        passwd = request.form['password']
+        t_db.signing_up(email,passwd)
+        flash('Registration successful','successful')
+        return redirect(url_for('signup'))
+    else:
+        return render_template('signup.html')
+
+@auth.route('/')
+def login():
+    return render_template('login.html')
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template, request, redirect,url_for, flash
 import taskmngr_db
 from actions import action
+from auth import auth
 from taskmngr_db import TaskDB, UserDB
 
 app = Flask(__name__)
@@ -10,8 +11,9 @@ app.config['taskdb'] = TaskDB()
 app.config['userdb'] = UserDB()
 
 app.register_blueprint(action,url_prefix='/')
+app.register_blueprint(auth,url_prefix='/')
 
-@app.route('/',methods=['POST','GET'])
+@app.route('/login',methods=['POST','GET'])
 def home():
     t_db = app.config['taskdb']
     if request.method == "POST":
@@ -21,22 +23,6 @@ def home():
     else:
         task_list = t_db.show_list()
         return render_template('main.html',task_list=task_list)
-
-@app.route('/sign',methods=['POST','GET'])
-def signup():
-    if request.method == 'POST':
-        t_db = app.config['userdb']
-        email = request.form['email']
-        passwd = request.form['password']
-        t_db.signing_up(email,passwd)
-        flash('Registration successful','success')
-        return redirect(url_for('signup'))
-    else:
-        return render_template('signup.html')
-
-@app.route('/log')
-def login():
-    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
